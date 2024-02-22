4baeb5616c33d711a120fe3b653ea7f7ca681551
MHoseinMohaghegh <129553891+MHoseinMohaghegh@users.noreply.github.com>
2024-02-22 18:04:40 +0330

    Update Project4_password_generator.py

--- Project4_password_generator.py
+++ Project4_password_generator.py
@@ -1,27 +1,37 @@
 # Password generator
+
+# importing modules
 import random
 from array import array
 import string
 
-a = int(input("Tedade raghamhaye password ra wared konid: "))
-b = int(input("Tedade horoof ra wared konid: "))
-c = a + b
-password = ["" for x in range(c)]
+# getting users input
+a = int(input("Enter the number of digits of the password: "))
+b = int(input("Enter the number of characters in the password: "))
+# defining the length of the password in the passwor list
+password = ["" for x in range(a + b)]
+# defining the length of the characters in the random_alphabet list
 random_alphabet = ["" for x in range(b)]
+# defining the length of the digits in the random_number list
 random_number = array('i', [0] * a)
-for number1 in range(a):
-    random_number[number1] = random.randint(0, 9)
+# choosing digits randomly, and put them into the random_number list
+for i in range(a):
+    random_number[i] = random.randint(0, 9)
     # print(random_number[number], end = "")
-for number2 in range(b):
-    random_alphabet[number2] = random.choice(string.ascii_letters)
+# choosing characters randomly(include upper case and lower case characters), and put them into the random_alphabet list
+for i in range(b):
+    random_alphabet[i] = random.choice(string.ascii_letters)
     # print(random_alphabet, end = "")
-for number3 in range(c):
-    if number3 < a:
-        password[number3] = random_number[number3]
+# putting digits and characters into the password list
+for i in range(len(password)):
+    if i < a:
+        password[i] = random_number[i]
     else:
-        password[number3] = random_alphabet[number3 - a]
-
-for number in range(c):
+        password[i] = random_alphabet[i - a]
+# displaying the password to the user by randomly selecting and removing elements from the password list
+for i in range(len(password)):
     x = random.choice(password)
-    print(x, end = "")
+    print(x, end="")
     password.remove(x)
+# Wait for user input before closing the program
+input()
