b1f64ec0f064e04d5026f2fa7d5c9427419107a8
MHoseinMohaghegh <129553891+MHoseinMohaghegh@users.noreply.github.com>
2024-02-24 18:24:12 +0330

    Update Project6_URL_Shortener.py

--- Project6_URL_Shortener.py
+++ Project6_URL_Shortener.py
@@ -1,14 +1,23 @@
+# Importing the pyshortener module for URL shortening
 import pyshorteners
 
-
+# Function to shorten a given URL
 def shortener(url):
-    s = pyshorteners.Shortener()
+    # Creating an instance of the Shortener class from pyshorteners
+    s = pyshorteners.Shortener() 
+    
+    # Using the tinyurl service to shorten the given URL and printing the result
     print(s.tinyurl.short(url))
 
-
+# Infinite loop for user input
 while True:
-    url = input('Please enter a url(for exit, type 0): ')
+    # Prompting the user to enter a URL, and providing instructions to exit by typing '0'
+    url = input('Please enter a url (for exit, type 0): ')
+    
+    # Checking if the user wants to exit the program
     if url == '0':
         break
     else:
+        # Calling the shortener function to shorten and print the URL
         shortener(url)
+
