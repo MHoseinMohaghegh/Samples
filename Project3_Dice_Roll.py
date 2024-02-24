ff362c32bf43bad9ad2c5757afc3f550cfe32798
MHoseinMohaghegh <129553891+MHoseinMohaghegh@users.noreply.github.com>
2024-02-24 18:25:52 +0330

    Update Project3_Dice_Roll.py

--- Project3_Dice_Roll.py
+++ Project3_Dice_Roll.py
@@ -1,9 +1,16 @@
+# importing modules
 import random
-from colorama import Fore
-
-
-x = [f"{Fore.RED}1", f"{Fore.BLUE}2", f"{Fore.RED}3", f"{Fore.BLUE}4", f"{Fore.RED}5", f"{Fore.BLUE}6"]
-y = int(input("Baraye tas andakhtan adade 1 ra wared konid: "))
+from colorama import Fore, init
+# using init method, with this method our program figure out the colorised strings when we creat an exe file
+init()
+# creating a list of colorised numbers for our dice
+x = [f"{Fore.RED}1", f"{Fore.BLUE}2", f"{Fore.RED}3",
+     f"{Fore.BLUE}4", f"{Fore.RED}5", f"{Fore.BLUE}6"]
+# get users input, and figure out that the user wants to dice or not
+y = int(input("Enter 1 to roll the dice: "))
+# if the usr enter "1" the program choose a number between 1 to 6, randomly
 if y == 1:
     random = random.choice(x)
     print(random)
+# get the user info before closing the program
+input()
