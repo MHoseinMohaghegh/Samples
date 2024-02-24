bb9cb0bdcee9adff5b4724441e68664a147e1027
MHoseinMohaghegh <129553891+MHoseinMohaghegh@users.noreply.github.com>
2024-02-24 18:25:24 +0330

    Update Project1_Shape.py

--- Project1_Shape.py
+++ Project1_Shape.py
@@ -1,70 +1,99 @@
+# Shape
+
+# importing modules
 import colorama
-from colorama import Fore
+from colorama import Fore, init
+# using init method, with this method our program figure out the colorised strings when we creat an exe file
+init()
+
+# by first function, program creates a square with totally green color
+
+
+def first_square_function():
+    for i in range(1, x+1):
+        for j in range(1, x+1):
+            square_1[i][j] = green_x
+
+# by second function, program creates a square with 2 triangle into it, and the left triangle is red
+
+
+def second_square_function():
+    for i in range(1, x + 1):
+        for j in range(1, i + 1):
+            square_2[i][j] = red_x
+        for k in range(j+1, x + 1):
+            square_2[i][k] = green_x
+
+# by third function, program creates a square with 2 triangle into it, and the left triangle is green
+
+
+def third_square_function():
+    y = x
+    for i in range(1, x+1):
+        y -= 1
+        for j in range(1, y+1):
+            square_3[i][j] = green_x
+        for k in range(y+1, x+1):
+            square_3[i][k] = red_x
+
+# by final function, program copy the first square to the final square, then checks all variables in the
+# second and third square, if the same variable in second or third square has red color, the program change
+# that variable in the final square to red color
+
+
+def final_square_function():
+    for i in range(1, x+1):
+        for j in range(1, x+1):
+            final_square[i][j] = square_1[i][j]
+            if square_2[i][j] == '\x1b[31mX' or square_3[i][j] == '\x1b[31mX':
+                final_square[i][j] = '\x1b[31mX'
 
 
 # '\x1b[31mX' = red
 # '\x1b[32mX' = green
-x = int(input("Lotfan tol e zele moraba ra wared konid: "))
-my_list_moraba = [[[] for _ in range(x+1)] for _ in range(x+1)]
-my_list1 = [[[] for _ in range(x+1)] for _ in range(x+1)]
-my_list2 = [[[] for _ in range(x+1)] for _ in range(x+1)]
-my_list_final = [[[] for _ in range(x+1)] for _ in range(x+1)]
-xxx = colorama.Fore.GREEN + "X"
-yyy = colorama.Fore.RED + "X"
-
-
-def moraba():
-    for number1 in range(1, x+1):
-        for number2 in range(1, x+1):
-            my_list_moraba[number1][number2] = xxx
-
-
-def mosalas1():
-    for number1 in range(1, x + 1):
-        for number2 in range(1, number1 + 1):
-            my_list1[number1][number2] = yyy
-        for number3 in range(number2+1, x + 1):
-            my_list1[number1][number3] = xxx
-
-
-def mosalas2():
-    y = x
-    for number1 in range(1, x+1):
-        y -= 1
-        for number2 in range(1, y+1):
-            my_list2[number1][number2] = xxx
-        for number3 in range(y+1, x+1):
-            my_list2[number1][number3] = yyy
-
-
-def final():
-    for number1 in range(1, x+1):
-        for number2 in range(1, x+1):
-            my_list_final[number1][number2] = my_list_moraba[number1][number2]
-            if my_list1[number1][number2] == '\x1b[31mX' or my_list2[number1][number2] == '\x1b[31mX':
-                my_list_final[number1][number2] = '\x1b[31mX'
-
-
-moraba()
+# get the length of the side of the square from the user
+x = int(input("Enter the length of the side of the square: "))
+# creating a list for our first square
+square_1 = [[[] for _ in range(x+1)] for _ in range(x+1)]
+# creating a list for our second square
+square_2 = [[[] for _ in range(x+1)] for _ in range(x+1)]
+# creating a list for our third square
+square_3 = [[[] for _ in range(x+1)] for _ in range(x+1)]
+# creating a list for our fourth square
+final_square = [[[] for _ in range(x+1)] for _ in range(x+1)]
+# creating variables with specific color
+green_x = colorama.Fore.GREEN + "X"
+red_x = colorama.Fore.RED + "X"
+# call the first function
+first_square_function()
+# print the first square
 for number1 in range(1, x+1):
     for number2 in range(1, x+1):
-        print(my_list_moraba[number1][number2], end="")
+        print(square_1[number1][number2], end="")
     print()
 print()
-mosalas1()
+# call the second square function
+second_square_function()
+# print the second square
 for number1 in range(1, x+1):
     for number2 in range(1, x+1):
-        print(my_list1[number1][number2], end="")
+        print(square_2[number1][number2], end="")
     print()
 print()
-mosalas2()
+# call the third square function
+third_square_function()
+# print the third square
 for number1 in range(1, x+1):
     for number2 in range(1, x+1):
-        print(my_list2[number1][number2], end="")
+        print(square_3[number1][number2], end="")
     print()
 print()
-final()
+# call the final square function
+final_square_function()
+# print the final square
 for number1 in range(1, x+1):
     for number2 in range(1, x+1):
-        print(my_list_final[number1][number2], end="")
+        print(final_square[number1][number2], end="")
     print()
+# get the user info before closing the program
+input()
